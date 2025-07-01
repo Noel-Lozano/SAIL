d76a4914d1db013871bd73fa24d55be45dbffbb2
Joey Zhou <sz545@cornell.edu>
2025-07-01 14:15:19 -0400

    Created genAI_API file to build pprompt and generate itinerary from Google Gemini 1.5-flash, and implemented main.py to print out finalized itinerary

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from weather_api import get_weather
+from genAI_api import generate_itinerary
 
 def main():
     city = input("Enter the city: ").strip()
@@ -6,4 +7,9 @@ def main():
     budget = input("Enter your budget: ").strip()
 
     weather = get_weather(city, date)
-    
+
+    itinerary = generate_itinerary(city, date, budget, weather)
+    print(itinerary)
+
+if __name__ == "__main__":
+    main()
